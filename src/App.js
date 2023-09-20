22f861319d61bfa8af99f3e60376b0cb8286031a
kavish-100403 <89000018+kavish-100403@users.noreply.github.com>
2023-09-20 23:28:19 +0530

    Added Favicons and added th functionality to change the title dynamically.

--- src/App.js
+++ src/App.js
@@ -27,12 +27,22 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "gray";
       showAlert("Dark mode has been enabled", "success");
+      document.title = 'TextUtils - Dark Mode'
+      //To show something at every few seconds we can use setInterval
+      // setInterval(() => {
+      //   document.title='TextUtils is Amazing'
+      // }, 1000);
+      // setInterval(() => {
+      //   document.title='Install TextUtils'
+      // }, 1500);
+    
       
     }
     else { 
       setMode('light');
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
+      document.title='TextUtils - Light Mode'
     }
   }
 
